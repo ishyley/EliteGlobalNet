09443e54f157189d0081e1be6b1020615d311e93
ishyley <phalayteesulaimpon@gmail.com>
2022-09-28 18:56:26 +0100

    Elite Global App

---
+++ app/src/main/java/com/example/eliteglobalnetwork/Eliteitem.java
@@ -0,0 +1,5 @@
+package com.example.eliteglobalnetwork;
+
+public class Eliteitem {
+
+}
---
+++ app/src/main/java/com/example/eliteglobalnetwork/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.eliteglobalnetwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://eliteglobalnetwork.net/");
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
